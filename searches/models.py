f49af822d719d1af785cdb0559652f90c0bb217b
Pulsvr <pulsvr@vk.com>
2025-12-17 20:54:26 +0500

    Создана модель поисков

--- searches/models.py
+++ searches/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Search(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    min_price = models.DecimalField(max_digits=10, decimal_places=2)
+    price_history = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
