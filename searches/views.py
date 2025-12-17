bbfbf91d3f5521fcaf957df3797d4bd145ff3309
isHardCoded <ishardcoded@gmail.com>
2025-12-17 18:07:53 +0300

    Сделали запрос продуктов DummyJSON

--- searches/views.py
+++ searches/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+URL = 'https://dummyjson.com/products'
+
+def get_data(url):
+  response = requests.get(url)
+  data = response.json()
+  return data
+
+def search(request):
+  products = get_data(URL)
+
+  print(products)
+
+  return render(request, 'searches/search.html', {
+    "products": products
+  })
